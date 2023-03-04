e3934644df00bca0a3c8ceb8142fe3e14039bd36
NathanHearts <nathanvhearts@gmail.com>
2023-03-04 14:21:31 +1100

    Code Refactor

--- Website/components.js
+++ Website/components.js
@@ -1,33 +1,3 @@
-const handleMovCheck = () => (
-    Array.from(document.getElementsByClassName('sliders')).forEach(s => (
-        document.getElementById('movementCheck').checked ? s.disabled = false : s.disabled = true
-    ))
-)
-function change_mode(){
-    console.log("Changed")
-    const radioButtons = document.querySelectorAll('input[name="control_mode"]');
-    control_div = document.getElementById('controls_div')
-    if (radioButtons[0].checked){
-        
-        control_div.innerHTML = `<input class="sliders" onmouseup="handleXServo()" id="xDeg" type="range" min="0"
-        max="255"></input>
-    <input class="sliders"onmouseup="handleYServo()" id="yDeg" type="range" min="0"
-        max="255"></input>`
-    }
-    else{  
-        control_div.innerHTML = `<div><input id="time_input" type="datetime-local"></input>
-        <input type="button" onclick='set_current_time()' value='Set to current Time'></input>
-        </div>`
-        
-    }
-}
-
-function set_current_time(){
-    const current = new Date()
-    const tzoff = current.getTimezoneOffset() * 60000
-    const ISO_time = new Date(current - tzoff).toISOString().slice(0,-1)
-    document.getElementById('time_input').value = ISO_time
-}
 const handleXServo = () => {
     // socket.emit()
     console.log(document.getElementById('xDeg').value)
@@ -37,4 +7,47 @@ const handleYServo = () => {
         socket.emit()
         console.log(document.getElementById('yDeg').value)
     }
+}
+
+function change_mode(){
+    console.log("Changed")
+    const radioButtons = document.querySelectorAll('input[name="control_mode"]');
+    control_div = document.getElementById('controls_div')
+    if (radioButtons[0].checked){
+        document.getElementById('autoControls').style.display = 'none'
+        document.getElementById('manualControls').style.display = 'block'
+    }
+    else {
+        document.getElementById('manualControls').style.display = 'none'
+        document.getElementById('autoControls').style.display = 'block'
+    }
+}
+
+const getLocation = () => (
+    new Promise((res,rej) => navigator.geolocation.getCurrentPosition(res,rej))
+)
+
+// const getLocation = () => (
+//     navigator.geolocation.getCurrentPosition(() => document.getElementById())
+// )
+
+
+//        const url = `api.meteomatics.com/${dateNow}/t_2m:C/${pos}/json`
+
+// const getLocation = () => {
+
+//     const url = 'api.meteomatics.com/2023-03-04T00:00:00Z--2023-03-07T00:00:00Z:PT1H/t_2m:C/52.520551,13.461804/json'
+//     return fetch(url, {
+//         headers: {
+//             'Authorization':'Basic' + ('monashuniversity_hay:3bNkBav5L1').toString('base64')
+//         }
+//     }).then(response => response.json()).then(json => console.log(json))
+// }
+
+
+function set_current_time(){
+    const current = new Date()
+    const tzoff = current.getTimezoneOffset() * 60000
+    const ISO_time = new Date(current - tzoff).toISOString().slice(0,-1)
+    document.getElementById('time_input').value = ISO_time
 }
