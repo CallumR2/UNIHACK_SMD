16c3d4277fd624dc2f4ebccae30aecaa07cdcd34
CallumR2 <101304812+CallumR2@users.noreply.github.com>
2023-03-05 16:21:56 +1100

    Update servo_controller.ino

--- arduino/servo_controller.ino
+++ arduino/servo_controller.ino
@@ -1,24 +1,32 @@
-#include <servo.h>
+#include <Servo.h>
 Servo myServo0, myServo1;
-int pMyservo = [2, 3];
+int pMyservo[] = {2, 3};
 int iServoPos = 0;
 
 void setup()
 {
-    myServo0.atttach(pMyServo[0]);
-    myServo1.atttach(pMyServo[1]);
-    Serial begin(9600)
+    myServo0.attach(pMyservo[0]);
+    myServo1.attach(pMyservo[1]);
+    //Initialisation
+    myServo0.write(90);
+    myServo1.write(90);
+    Serial.begin(9600);
 }
-Void loop()
+
+void loop()
 {
-     int servo_input = [0, 0];
+    char servo_input[] = {'0', '0'};
+    //Reads one byte at a time from Serial
+    //Serial will give two angles, already pre-proccessed via website source
+    //Received angles just need to be written to Servos!
     if (Serial.available() > 0) {
-        for(i = 0; i < 2; i++){
-            servo_input[i] = Serial.read()/255 * 180
+        for(int i = 0; i < 2; i++){
+            servo_input[i] = Serial.read();
         }
     }
-    myServo0.write(servo_input[0]);
-    myServo1.write(servo_input[1]);
+    myServo0.write(int(servo_input[0]));
+    myServo1.write(int(servo_input[1]));
     delay(1000); //For Testing Purposes
     
 }
+
