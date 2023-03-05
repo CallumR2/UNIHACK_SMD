abd288b7feab1c80b9f7de89194cef8d58462e47
CallumR2 <101304812+CallumR2@users.noreply.github.com>
2023-03-05 12:26:10 +1100

    Website Fixes

--- Website/src/components.js
+++ Website/src/components.js
@@ -32,10 +32,11 @@ const getLocation = () => (
 // )
 
 const handleSunDirect = () => {
+    
     document.getElementById('azimuth').value = sunDirections(
         document.getElementById('apiTokenDiv').innerHTML,
-        document.getElementById('locationDiv').innerHTML,
-        document.getElementById('timeInputDiv').innerHTML
+        document.getElementById('locationDiv').innerHTML, 
+        new Date(document.getElementById('time_input').value).toISOString()
     )
 }
 
@@ -43,15 +44,16 @@ const sunDirections = async (apiTok,location,dateTime) => {
     const url = `https://api.meteomatics.com/${dateTime}/sun_azimuth:d,sun_elevation:d/${location}/json?access_token=${apiTok}`
     fetch(url).then(res => res.json()).then(res => {
         console.log(res.data[0].coordinates[0].dates[0])
-        document.getElementById('azimuth').innerHTML = res.data[0].coordinates[0].dates[0].value
-        document.getElementById('elevation').innerHTML = res.data[1].coordinates[0].dates[0].value
+        document.getElementById('azimuth').innerHTML = "Azimuth: " + res.data[0].coordinates[0].dates[0].value
+        document.getElementById('elevation').innerHTML = "Elevation: " + res.data[1].coordinates[0].dates[0].value
     }).catch(err => console.log('Sun API failed with error: '+err))
 }
 
 function handle_time_input() {
-    document.getElementById('time_input') .disabled = document.getElementById("time_check").checked
+    document.getElementById('time_input').disabled = document.getElementById("time_check").checked
     set_current_time()
 }
+
 
 function set_current_time() {
     time_input = document.getElementById('time_input')
@@ -59,10 +61,12 @@ function set_current_time() {
         const current = new Date()
         const tzoff = current.getTimezoneOffset() * 60000
         const ISO_time = new Date(current - tzoff).toISOString().slice(0, -3)
-        time_input.value = ISO_time.slice(0,-3)+'Z'
-        document.getElementById('timeInputDiv').innerHTML = current.toISOString()
+        time_input.value = ISO_time.slice(0,-2)
+        console.log(ISO_time.slice(0,-2))
     }
 }
+
+
 
 const getAPILogin = async () => (
     fetch('https://login.meteomatics.com/api/v1/token', {method: 'GET', headers: {'Authorization':'Basic ' + window.btoa('monashuniversity_hay:3bNkBav5L1')}
