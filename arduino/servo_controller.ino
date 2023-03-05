5c89f617790a5191747df58ceb80acca18524c56
CallumR2 <101304812+CallumR2@users.noreply.github.com>
2023-03-05 11:47:50 +1100

    update servo controll

--- arduino/servo_controller.ino
+++ arduino/servo_controller.ino
@@ -1,19 +1,24 @@
 #include <servo.h>
-Servo myServo;
+Servo myServo0, myServo1;
 int pMyservo = [2, 3];
 int iServoPos = 0;
 
 void setup()
 {
-    myServo1.atttach(pMyServo[0]);
-    myServo.atttach(pMyServo[1]);
+    myServo0.atttach(pMyServo[0]);
+    myServo1.atttach(pMyServo[1]);
+    Serial begin(9600)
 }
 Void loop()
 {
-    myServo.write(0);
-    delay(2000);
-    myServo.write(90);
-    delay(2000);
-    myServo.write(180);
-    delay(2000);
+     int servo_input = [0, 0];
+    if (Serial.available() > 0) {
+        for(i = 0; i < 2; i++){
+            servo_input[i] = Serial.read()/255 * 180
+        }
+    }
+    myServo0.write(servo_input[0]);
+    myServo1.write(servo_input[1]);
+    delay(1000); //For Testing Purposes
+    
 }
